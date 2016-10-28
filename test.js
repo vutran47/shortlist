0c4c50a5ecf4b5393ace93bac04afe1c7011346b
Elderaine <vutran.1710@gmail.com>
2016-10-28 13:29:18 +0700

    check N is NaN

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 'use strict'
 var fs = require('fs');
-var input_array = [];
+//var input_array = [];
 
 function my_func(N_array) {
   for (let Nx of N_array) {
     let N = parseInt(Nx);
     let count = 0;
     let a, b, c;
-    if (N < 4) continue;
+    if (N < 4 || isNaN(N)) continue;
       for (a = Math.ceil(N / 4); a <= N - 3; a++) {
           for (b = Math.ceil((N - a) / 3); b <= Math.min(a, N - 2 - a); b++) {
               for (c = Math.ceil((N - a - b) / 2); c <= Math.min(b, N - 1 - a - b); c++) {
